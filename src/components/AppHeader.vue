c51312542f94fc3e94892e6eab23b27c34446fb1
Rivio98 <rivaldogjoni1998@gmail.com>
2024-08-31 16:06:22 +0200

    [UPD] Add hover for the icons

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -17,22 +17,22 @@ export default {
     <div class='container'>
         <div class="row py-3">
             <div class="col-6 d-flex">
-                <div class="content text-light d-flex">
+                <div class="content text-light d-flex align-items-center">
                     <i class="bi bi-envelope"></i>
                     <p class="ps-1">Superhit Top Movie</p>
                     <p class="txt-green ps-1">**King Star**</p>
                 </div>
-                <div class="content icons text-light d-flex ps-3">
-                    <i class="bi bi-facebook ps-2"></i>
-                    <i class="bi bi-twitter ps-2"></i>
-                    <i class="bi bi-linkedin ps-2"></i>
-                    <i class="bi bi-instagram ps-2"></i>
-                    <i class="bi bi-google ps-2">+</i>
+                <div class="content-icons text-light d-flex ps-3 align-items-center justify-content-center">
+                    <i class="fa-brands fa-facebook-f"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                    <i class="fa-brands fa-linkedin-in"></i>
+                    <i class="fa-brands fa-instagram"></i>
+                    <i class="fa-brands fa-google-plus-g"></i>
                 </div>
             </div>
             <div class="col-6 align-items-center">
-                <div class="content icons d-flex text-light justify-content-end">
-                    <i class="bi bi-lock-fill"></i>
+                <div class="content-icons d-flex text-light justify-content-end align-items-center">
+                    <i class="fa-solid fa-lock"></i>
                     <p class="ps-1">Login</p>
                 </div>
             </div>
@@ -123,5 +123,64 @@ export default {
     font-size: 12px;
     line-height: 1;
     text-align: center;
+}
+
+.content-icons i {
+    margin-left: 15px;
+    font-size: 15px;
+    padding: 7px 10px;
+}
+
+i:hover {
+    color: #fff;
+    background-color: transparent;
+    border-radius: 50%;
+    width: 27px;
+    height: 27px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.fa-facebook-f {
+    color: #4267B2;
+}
+
+.fa-facebook-f:hover {
+    background-color: #4267B2;
+}
+
+.fa-twitter {
+    color: #1DA1F2;
+}
+
+.fa-twitter:hover {
+    background-color: #1DA1F2;
+}
+
+.fa-linkedin-in {
+    color: #0A66C2;
+}
+
+.fa-linkedin-in:hover {
+    background-color: #0A66C2;
+
+}
+
+.fa-instagram {
+    color: #FD1D1D;
+}
+
+.fa-instagram:hover {
+    background-color: #FD1D1D;
+
+}
+
+.fa-google-plus-g {
+    color: #db4a39;
+}
+
+.fa-google-plus-g:hover {
+    background-color: #db4a39;
 }
 </style>
