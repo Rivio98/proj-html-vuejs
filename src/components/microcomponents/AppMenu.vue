5d4c824e18951dcd35ad8c416177eeabd7996934
Rivio98 <rivaldogjoni1998@gmail.com>
2024-08-31 19:46:42 +0200

    [UPD] Try to fix menu issue

--- src/components/microcomponents/AppMenu.vue
+++ src/components/microcomponents/AppMenu.vue
@@ -1,32 +1,71 @@
 <script>
 
+import { store } from '../../store.js';
+
 export default {
-    data() {
-        return {
-        };
-    },
-    props: {
-        menu: Object
-    },
-
-    methods: {
-
+    setup() {
+        return { store };
     }
 };
 </script>
 
 <template>
-    <li><a href="">{{ menu.title }}</a><i class="bi bi-caret-down-fill align-middle" v-if="menu.dropdown"></i></li>
+    <div class="menu-container">
+        <ul class="nav">
+            <li v-for="menu in store.menus" :key="menu.title" class="nav-item dropdown">
+                <a v-if="menu.dropdown" href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"
+                    aria-expanded="false">
+                    {{ menu.title }}
+                    <i class="bi bi-caret-down-fill align-middle"></i>
+                </a>
+                <a v-else href="#" class="nav-link">
+                    {{ menu.title }}
+                </a>
+                <ul v-if="menu.dropdown" class="dropdown-menu">
+                    <li v-for="item in menu.dropdown" :key="item" class="dropdown-item">
+                        {{ item }}
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <style scoped>
-a {
-    text-decoration: none;
+.menu-container {
+    padding: 10px;
+}
+
+.nav-item {
+    position: relative;
+}
+
+.nav-link {
+    color: #fff;
+    padding: 0.5rem 1rem;
+}
+
+.nav-link:hover {
+    color: #13be13;
+}
+
+.dropdown-menu {
+    background-color: #060f19;
+    border-radius: 0.375rem;
+    box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075);
+}
+
+.dropdown-item {
+    color: #fff;
+    padding: 0.5rem 1rem;
+}
+
+.dropdown-item:hover {
+    background-color: #13be13;
     color: #fff;
 }
 
-li {
-    padding: 0px 20px 0px 0px;
-    font-weight: bold;
+.bi-caret-down-fill {
+    margin-left: 0.5rem;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,59 +1,14 @@
-import { reactive } from "vue";
+import { reactive } from 'vue';
 
 export const store = reactive({
     menus: [
-        {
-            title: "Home",
-            arrow: true,
-            dropdown: [
-                "Home",
-                "Home",
-                "Home"
-            ]
-        },
-        {
-            title: "Celebrity",
-            arrow: true,
-            dropdown: [
-                "Celebrity",
-                "Celebrity",
-                "Celebrity"
-            ]
-        },
-        {
-            title: "Movie",
-            dropdown: [
-                "Movie",
-                "Movie",
-                "Movie"
-            ]
-        },
-        {
-            title: "Page",
-            dropdown: [
-                "Page",
-                "Page",
-                "Page"
-            ]
-        },
-        {
-            title: "Shop",
-            dropdown: [
-                "Shop",
-                "Shop",
-                "Shop"
-            ]
-        },
-        {
-            title: "Blog",
-            dropdown: false,
-        },
-        {
-            title: "Contact Us",
-            dropdown: false,
-        }
-    ],
+        { title: "Home", dropdown: ["Home", "Home", "Home"] },
+        { title: "Celebrity", dropdown: ["Celebrity", "Celebrity", "Celebrity"] },
+        { title: "Movie", dropdown: ["Movie", "Movie", "Movie"] },
+        { title: "Page", dropdown: ["Page", "Page", "Page"] },
+        { title: "Shop", dropdown: ["Shop", "Shop", "Shop"] },
+        { title: "Blog" },
+        { title: "Contact Us" }
+    ]
 });
-
-
 
