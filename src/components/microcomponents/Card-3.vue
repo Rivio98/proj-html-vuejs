eec87a2d7367e338055f7740bc9a6d3ef4e4c21f
Rivio98 <rivaldogjoni1998@gmail.com>
2024-09-01 20:55:03 +0200

    [UPD] Add card type 3

---
+++ src/components/microcomponents/Card-3.vue
@@ -0,0 +1,340 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+    <div class="container">
+        <div class="row row-cols-3 mt-100 flex-wrap">
+            <div class="col py-3 px-3">
+                <div class="cd">
+                    <div class="image-wrapper">
+                        <img src="../../assets/1.jpg" alt="Movie Image">
+                    </div>
+                    <div class="content-wrapper">
+                        <div class="score text-end mt-3 pe-3 d-flex justify-content-between w-100">
+                            <div class="lable-top">2.30.0</div>
+                            <i class="fa-solid fa-star"><span class="text-light ps-1">0/10</span></i>
+                        </div>
+                        <div class="title text-light ps-3">
+                            <div class="play d-flex justify-content-center py-5">
+                                <i class="fa-regular fa-circle-play"></i>
+                            </div>
+                            <h3>Hurry Animate Blue Strack New Movie</h3>
+                            <h5 class="py-2">Category: Top Rating</h5>
+                            <p>Release: February 12,2016</p>
+                            <p>Genres: Drama,Action Releas</p>
+                        </div>
+                        <div class="lables d-flex">
+                            <div class="label-left">
+                                <h5>Details</h5>
+                            </div>
+                            <div class="lable-right">
+                                <h5>381 Views</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col py-3 px-3">
+                <div class="cd">
+                    <div class="image-wrapper">
+                        <img src="../../assets/5 (1).jpg" alt="Movie Image">
+                    </div>
+                    <div class="content-wrapper">
+                        <div class="score text-end mt-3 pe-3 d-flex justify-content-between w-100">
+                            <div class="lable-top">2.30.0</div>
+                            <i class="fa-solid fa-star"><span class="text-light ps-1">0/10</span></i>
+                        </div>
+                        <div class="title text-light ps-3">
+                            <div class="play d-flex justify-content-center py-5">
+                                <i class="fa-regular fa-circle-play"></i>
+                            </div>
+                            <h3>Hurry Animate Blue Strack New Movie</h3>
+                            <h5 class="py-2">Category: Top Rating</h5>
+                            <p>Release: February 12,2016</p>
+                            <p>Genres: Drama,Action Releas</p>
+                        </div>
+                        <div class="lables d-flex">
+                            <div class="label-left">
+                                <h5>Details</h5>
+                            </div>
+                            <div class="lable-right">
+                                <h5>381 Views</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col py-3 px-3">
+                <div class="cd">
+                    <div class="image-wrapper">
+                        <img src="../../assets/3.jpg" alt="Movie Image">
+                    </div>
+                    <div class="content-wrapper">
+                        <div class="score text-end mt-3 pe-3 d-flex justify-content-between w-100">
+                            <div class="lable-top">2.30.0</div>
+                            <i class="fa-solid fa-star"><span class="text-light ps-1">0/10</span></i>
+                        </div>
+                        <div class="title text-light ps-3">
+                            <div class="play d-flex justify-content-center py-5">
+                                <i class="fa-regular fa-circle-play"></i>
+                            </div>
+                            <h3>Hurry Animate Blue Strack New Movie</h3>
+                            <h5 class="py-2">Category: Top Rating</h5>
+                            <p>Release: February 12,2016</p>
+                            <p>Genres: Drama,Action Releas</p>
+                        </div>
+                        <div class="lables d-flex">
+                            <div class="label-left">
+                                <h5>Details</h5>
+                            </div>
+                            <div class="lable-right">
+                                <h5>381 Views</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col py-3 px-3">
+                <div class="cd">
+                    <div class="image-wrapper">
+                        <img src="../../assets/4.jpg" alt="Movie Image">
+                    </div>
+                    <div class="content-wrapper">
+                        <div class="score text-end mt-3 pe-3 d-flex justify-content-between w-100">
+                            <div class="lable-top">2.30.0</div>
+                            <i class="fa-solid fa-star"><span class="text-light ps-1">0/10</span></i>
+                        </div>
+                        <div class="title text-light ps-3">
+                            <div class="play d-flex justify-content-center py-5">
+                                <i class="fa-regular fa-circle-play"></i>
+                            </div>
+                            <h3>Hurry Animate Blue Strack New Movie</h3>
+                            <h5 class="py-2">Category: Top Rating</h5>
+                            <p>Release: February 12,2016</p>
+                            <p>Genres: Drama,Action Releas</p>
+                        </div>
+                        <div class="lables d-flex">
+                            <div class="label-left">
+                                <h5>Details</h5>
+                            </div>
+                            <div class="lable-right">
+                                <h5>381 Views</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col py-3 px-3">
+                <div class="cd">
+                    <div class="image-wrapper">
+                        <img src="../../assets/3-2.jpg" alt=" Movie Image">
+                    </div>
+                    <div class="content-wrapper">
+                        <div class="score text-end mt-3 pe-3 d-flex justify-content-between w-100">
+                            <div class="lable-top">2.30.0</div>
+                            <i class="fa-solid fa-star"><span class="text-light ps-1">0/10</span></i>
+                        </div>
+                        <div class="title text-light ps-3">
+                            <div class="play d-flex justify-content-center py-5">
+                                <i class="fa-regular fa-circle-play"></i>
+                            </div>
+                            <h3>Hurry Animate Blue Strack New Movie</h3>
+                            <h5 class="py-2">Category: Top Rating</h5>
+                            <p>Release: February 12,2016</p>
+                            <p>Genres: Drama,Action Releas</p>
+                        </div>
+                        <div class="lables d-flex">
+                            <div class="label-left">
+                                <h5>Details</h5>
+                            </div>
+                            <div class="lable-right">
+                                <h5>381 Views</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col py-3 px-3">
+                <div class="cd">
+                    <div class="image-wrapper">
+                        <img src="../../assets/2.jpg" alt="Movie Image">
+                    </div>
+                    <div class="content-wrapper">
+                        <div class="score text-end mt-3 pe-3 d-flex justify-content-between w-100">
+                            <div class="lable-top">2.30.0</div>
+                            <i class="fa-solid fa-star"><span class="text-light ps-1">0/10</span></i>
+                        </div>
+                        <div class="title text-light ps-3">
+                            <div class="play d-flex justify-content-center py-5">
+                                <i class="fa-regular fa-circle-play"></i>
+                            </div>
+                            <h3>Hurry Animate Blue Strack New Movie</h3>
+                            <h5 class="py-2">Category: Top Rating</h5>
+                            <p>Release: February 12,2016</p>
+                            <p>Genres: Drama,Action Releas</p>
+                        </div>
+                        <div class="lables d-flex">
+                            <div class="label-left">
+                                <h5>Details</h5>
+                            </div>
+                            <div class="lable-right">
+                                <h5>381 Views</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.b-green {
+    width: 100%;
+    border-left: 4px solid #13BE13;
+    border-bottom: 2px solid #182028;
+}
+
+.cd {
+    width: 100%;
+    height: 500px;
+    position: relative;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    box-sizing: border-box;
+    transition: transform 0.3s ease;
+}
+
+.cd:hover {
+    cursor: pointer;
+}
+
+.image-wrapper {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+
+.image-wrapper::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(45deg, rgba(19, 190, 19, 0.2) 0%, rgba(19, 190, 19, 0.6) 100%);
+    opacity: 0;
+    transition: opacity 0.3s ease;
+    z-index: 1;
+}
+
+.cd:hover .image-wrapper::before {
+    opacity: 1;
+}
+
+.image-wrapper img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    filter: opacity(50%);
+}
+
+.content-wrapper {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 2;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    box-sizing: border-box;
+}
+
+.score {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+}
+
+.title {
+    color: white;
+    margin-top: auto;
+    text-align: start;
+}
+
+.lables {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+}
+
+.label-left,
+.lable-right {
+    color: white;
+    background-color: #060F19;
+    padding: 5px 15px;
+}
+
+.label-left {
+    border-radius: 0px 20px 20px 0px;
+}
+
+.lable-right {
+    border-radius: 20px 0px 0px 20px;
+}
+
+.lable-top {
+    color: white;
+    background-color: #060F19;
+    padding: 5px 15px;
+    border-radius: 0px 20px 20px 0px;
+    visibility: hidden;
+}
+
+.fa-star {
+    color: #15BE14;
+}
+
+.fa-angles-left,
+.fa-angles-right {
+    padding: 5px;
+    color: #13BE13;
+    border-radius: 50%;
+    border: 2px solid #182028;
+    width: 30px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.title p {
+    display: block;
+    opacity: 0;
+    visibility: hidden;
+    transition: opacity 0.3s ease, visibility 0.3s ease;
+}
+
+.cd:hover .title p {
+    opacity: 1;
+    visibility: visible;
+}
+
+.cd:hover .lable-top {
+    visibility: visible;
+}
+
+.cd:hover .play i {
+    display: block;
+}
+
+.play i {
+    display: none;
+    font-size: 50px;
+}
+</style>
