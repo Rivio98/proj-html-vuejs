57951fe66834f3d0a38bd4f8a9a3a1ae2d3b780b
Rivio98 <rivaldogjoni1998@gmail.com>
2024-09-01 20:52:24 +0200

    [UPD] Added card type 2

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,13 +3,15 @@ import Jumbotron1 from './microcomponents/Jumbotron-1.vue';
 import VideoPlayer from './microcomponents/VideoPlayer.vue';
 import Jumbotron2 from './microcomponents/Jumbotron-2.vue';
 import Card1 from './microcomponents/Card-1.vue';
+import Card2 from './microcomponents/Card-2.vue';
 
 export default {
     components: {
         Jumbotron1,
         Card1,
         VideoPlayer,
-        Jumbotron2
+        Jumbotron2,
+        Card2
 
     },
 }
@@ -19,6 +21,7 @@ export default {
     <Card1 />
     <VideoPlayer />
     <Jumbotron2 />
+    <Card2 />
 </template>
 
 <style lang="">
