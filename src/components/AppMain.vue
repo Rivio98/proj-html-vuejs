671ca5823cd652a11c8de0fd3c9cdb1d12c4c672
Rivio98 <rivaldogjoni1998@gmail.com>
2024-09-01 18:50:32 +0200

    [UPD] Add video player component

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,13 @@
 <script>
 import Jumbotron1 from './microcomponents/Jumbotron-1.vue';
+import VideoPlayer from './microcomponents/VideoPlayer.vue';
 import Card1 from './microcomponents/Card-1.vue';
 
 export default {
     components: {
         Jumbotron1,
-        Card1
+        Card1,
+        VideoPlayer
 
     },
 }
@@ -13,6 +15,7 @@ export default {
 <template>
     <Jumbotron1 />
     <Card1 />
+    <VideoPlayer />
 </template>
 
 <style lang="">
---
+++ src/components/microcomponents/VideoPlayer.vue
@@ -0,0 +1,216 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+    <div class="container mt-100 mb-3">
+        <div class="row pt-3">
+            <div class="col-12">
+                <div class="content text-light d-flex justify-content-between">
+                    <div class="d-flex justify-content-between w-100">
+                        <div class="ps-3 pb-2 b-green">
+                            <h3>Movie Playlist</h3>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni aut at.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row py-5">
+            <div class="col-8">
+                <div class="content bg-black text-light d-flex flex-column justify-content-between">
+                    <div class="top-black">
+                        <div class="content p-3 d-flex align-items-center justify-content-between">
+                            <div class="d-flex align-items-center">
+                                <img class="logo" src="../../assets/channels4_profile.jpg" alt="">
+                                <p class="px-2">Yeh Saali Aashiqui|Official Trailer|Vardan Puri,Shivaleek...</p>
+                            </div>
+                            <div class="text-center d-flex">
+                                <div class="px-3">
+                                    <i class="fa-solid fa-clock"></i>
+                                    <p>Guarda di più...</p>
+                                </div>
+                                <div class="px-3">
+                                    <i class="fa-solid fa-share"></i>
+                                    <p>Condividi</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bottom-black">
+                        <div class="content-logo d-flex">
+                            <p>Guarda su </p><i class="fa-brands fa-youtube ps-2"></i>
+                            <p>YOUTUBE</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="top-section text-light p-1">
+                    <div class="content ps-2">
+                        <h4>New Movie</h4>
+                        <h5>Playing 24</h5>
+                    </div>
+                </div>
+                <div class="bottom-section text-light">
+                    <div class="content-bottom row row-cols-1 d-flex flex-column">
+                        <div class="col">
+                            <div class="content">
+                                <div class="row p-2">
+                                    <div class="col-4 m-0 p-0">
+                                        <div class="content">
+                                            <img src="../../assets/2-100x100.jpg" alt="" style="width: 100%;"
+                                                class="pt-2">
+                                        </div>
+                                    </div>
+                                    <div class="col-8 m-0">
+                                        <div class="content">
+                                            <h4>Quisque Auctor Movie in Strack</h4>
+                                            <h5>947 Views</h5>
+                                            <div class="d-flex">
+                                                <p>February 12,2016</p>
+                                                <p class="ps-4">2.20.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="content">
+                                <div class="row p-2">
+                                    <div class="col-4 m-0 p-0">
+                                        <div class="content">
+                                            <img src="../../assets/5-100x100.jpg" alt="" style="width: 100%;"
+                                                class="pt-2">
+                                        </div>
+                                    </div>
+                                    <div class="col-8 m-0">
+                                        <div class="content">
+                                            <h4>Quisque Auctor Movie in Strack</h4>
+                                            <h5>947 Views</h5>
+                                            <div class="d-flex">
+                                                <p>February 12,2016</p>
+                                                <p class="ps-4">2.20.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="content">
+                                <div class="row p-2">
+                                    <div class="col-4 m-0 p-0">
+                                        <div class="content">
+                                            <img src="../../assets/3-100x100.jpg" alt="" style="width: 100%;"
+                                                class="pt-2">
+                                        </div>
+                                    </div>
+                                    <div class="col-8 m-0">
+                                        <div class="content">
+                                            <h4>Quisque Auctor Movie in Strack</h4>
+                                            <h5>947 Views</h5>
+                                            <div class="d-flex">
+                                                <p>February 12,2016</p>
+                                                <p class="ps-4">2.20.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="content">
+                                <div class="row p-2">
+                                    <div class="col-4 m-0 p-0">
+                                        <div class="content">
+                                            <img src="../../assets/3-1-100x100.jpg" alt="" style="width: 100%;"
+                                                class="pt-2">
+                                        </div>
+                                    </div>
+                                    <div class="col-8 m-0">
+                                        <div class="content">
+                                            <h4>Quisque Auctor Movie in Strack</h4>
+                                            <h5>947 Views</h5>
+                                            <div class="d-flex">
+                                                <p>February 12,2016</p>
+                                                <p class="ps-4">2.20.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="content">
+                                <div class="row p-2">
+                                    <div class="col-4 m-0 p-0">
+                                        <div class="content">
+                                            <img src="../../assets/4-1-100x100.jpg" alt="" style="width: 100%;"
+                                                class="pt-2">
+                                        </div>
+                                    </div>
+                                    <div class="col-8 m-0">
+                                        <div class="content">
+                                            <h4>Quisque Auctor Movie in Strack</h4>
+                                            <h5>947 Views</h5>
+                                            <div class="d-flex">
+                                                <p>February 12,2016</p>
+                                                <p class="ps-4">2.20.00</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.b-green {
+    width: 100%;
+    border-left: 4px solid #13BE13;
+    border-bottom: 2px solid #182028;
+}
+
+.bg-black {
+    background-color: black;
+    height: 500px;
+}
+
+.top-section {
+    height: 80px;
+    background-color: #182028;
+    margin-bottom: 2px;
+}
+
+.bottom-section {
+    background-color: #182028;
+    height: 418px;
+    overflow-y: auto;
+    padding: 1rem;
+}
+
+.logo {
+    width: 50px;
+    border-radius: 50%;
+}
+
+i {
+    font-size: x-large;
+}
+
+.content-logo {
+    width: 200px;
+    padding: 10px;
+    background-color: #182028;
+    margin-bottom: 10px;
+    align-items: center;
+}
+</style>
